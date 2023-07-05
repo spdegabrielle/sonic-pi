690e4c02d971f622655caace10289dabec663ab6
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2023-07-05 11:28:15 +0100

    Update info.rkt add to  ("Audio") category on docs homepage

--- sonic-pi/info.rkt
+++ sonic-pi/info.rkt
@@ -2,7 +2,7 @@
 
 (define name "Sonic PI")
 
-(define scribblings '(("sonic-pi.scrbl" () (tool))))
+(define scribblings '(("sonic-pi.scrbl" () ("Audio"))))
 
 (define compile-omit-paths '())
 (define test-omit-paths '("examples"))
